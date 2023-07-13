7eef99664dd29957fc9549548112339fef733e20
dreamer <dreamer@puikheid.nl>
2023-07-13 17:10:36 +0200

    add dark colors

--- wstdcolors.hpp
+++ wstdcolors.hpp
@@ -3,13 +3,16 @@
 
 auto Blue     = ImColor::HSV(2.04f / 3.6f, 0.83f, 0.64f);
 auto BlueBr   = ImColor::HSV(2.04f / 3.6f, 0.83f, 0.84f);
+auto BlueDr   = ImColor::HSV(2.04f / 3.6f, 0.83f, 0.44f);
 auto Green    = ImColor::HSV(1.6f  / 3.6f, 0.77f, 0.64f);
 auto GreenBr  = ImColor::HSV(1.6f  / 3.6f, 0.77f, 0.74f);
 auto GreenDr  = ImColor::HSV(1.6f  / 3.6f, 0.77f, 0.44f);
 auto Red      = ImColor::HSV(0.03f / 3.6f, 0.76f, 0.74f);
 auto RedBr    = ImColor::HSV(0.03f / 3.6f, 0.76f, 0.84f);
+auto RedDr    = ImColor::HSV(0.03f / 3.6f, 0.76f, 0.54f);
 auto Yellow   = ImColor::HSV(0.47f / 3.6f, 0.75f, 0.64f);
 auto YellowBr = ImColor::HSV(0.47f / 3.6f, 0.75f, 0.84f);
+auto YellowDr = ImColor::HSV(0.47f / 3.6f, 0.75f, 0.44f);
 auto Grey     = ImVec4(0.3f, 0.3f, 0.3f, 1.0f);
 auto GreyBr   = ImVec4(0.4f, 0.4f, 0.4f, 1.0f);
 auto WhiteDr  = ImVec4(0.8f, 0.8f, 0.8f, 0.8f);
